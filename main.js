dfd29bfdaf1d4b9b15da0132be0d5cb3e4d8e5a9
Aar102211 <95637641+Aar102211@users.noreply.github.com>
2022-07-05 03:26:31 -1100

    Add files via upload

--- main.js
+++ main.js
@@ -23,7 +23,27 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  poseNet =ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+
+
 }
+
+function modelLoaded()
+{
+  console.log("Posenet is initialized!");
+}
+
+function gotPoses()
+{
+  
+}
+
+
 
 
 function draw(){
